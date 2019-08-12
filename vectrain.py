fb8a46727bcd24e12b02d5a6647827146f47ba27
zhengyaogu <zhengyao.gu30@gmail.com>
2019-08-12 13:49:57 -0700

    merge util.pu

--- vectrain.py
+++ vectrain.py
@@ -1,13 +1,12 @@
 from util import delete_last_line
-<<<<<<< HEAD
 import pprint
-=======
 import pandas as pd
 import os
 import random
 import seaborn as sns
 from compare import getExampleSentencesBySense
 import matplotlib.pyplot as plt
+import torch
 
 
 #Note: 12 is the largest valid indice
@@ -156,6 +155,22 @@ def present_csv_DF_data(good_threshold, bad_threshold, num_example_sentences):
             if row[1] == lemma:
                 printSentences(getExampleSentencesBySense(row[3], 3))
                 printSentences(getExampleSentencesBySense(row[4], 3))
+
+classifier_data_#
+spec, lemma, best_avg_acc, sense1, sense2
+def high_acc_test(threshold_high, threshold_low, filename):
+    df = pd.read_csv(filename)
+    high_acc_df = df[df["best_avg_acc"] >= threshold_high]
+    low_acc_df = df[df["best_avg_acc"] <= threshold_low]
+    data = pd.concat([high_acc_df, low_acc_df]).sample(frac=1)
+    
+    high_stats = []
+    low_stats = []
+    for i in data.index:
+        pos1 = 
+
+
+
 
 
 
