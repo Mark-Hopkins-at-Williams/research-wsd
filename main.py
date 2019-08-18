0204dcec7464124bd60d213cef6859b6e60c7706
zhengyaogu <zhengyao.gu30@gmail.com>
2019-08-18 16:01:38 -0700

    write the result of training to finetune_result.txt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from experiment import train_finetune
-
+import json
 if __name__ == "__main__":
-    train_finetune(20,1000000000,10,1000)
+    result = train_finetune(20,1000000000,10,1000)
+    with open("finetune_result.txt", "w") as f:
+        json.dump(result, f)
 
