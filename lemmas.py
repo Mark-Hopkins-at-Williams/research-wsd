7370b766c07fa36bec7abb6f95c0330f2d4711f2
zhengyaogu <zhengyao.gu30@gmail.com>
2019-08-18 16:46:10 -0700

    train_lemma_classifier_with_vec without read and write into bert.py

--- lemmas.py
+++ lemmas.py
@@ -134,6 +134,40 @@ def contextualized_vectors_by_sense(lemma, use_cached = False):
         sense_vectors[sense].append(vec)
     return dict(sense_vectors)    
 
+def contextualized_vectors_by_sense_with_vec(lemma, vectorize, use_cached = False):
+    """
+    Returns a dictionary mapping each word sense (of a particular lemma)
+    to a list of vector encodings of its SenseInstances.
+    
+    If use_cached == True, then this assumes that a file called
+    `lemmadata/vectors/LEMMA.csv` exists and contains the serialized
+    vectors in CSV format. If use_cached == False, the vectors are
+    computed from scratch.
+    
+    """
+    def cached_contextualized_vectors(lemma):
+        filename = join("lemmadata/vectors", lemma + ".csv")
+        data = pd.read_csv(filename, delimiter=",")
+        for k in range(len(data.index)):
+            curr = data.iloc[k]
+            yield (curr.iloc[2], torch.tensor(curr.iloc[4:]))
+    
+    def contextualized_vectors(lemma):    
+        for instance in lemmadata(lemma):
+            # Bert can only handle sentences with a maximum of 512 tokens
+            if len(instance.tokens) > 511:
+                continue
+            yield (instance.sense, vectorize(instance))
+
+    if use_cached:
+        context_vecs = cached_contextualized_vectors
+    else:
+        context_vecs = contextualized_vectors
+    sense_vectors = defaultdict(list)
+    for (sense, vec) in context_vecs(lemma):
+        sense_vectors[sense].append(vec)
+    return dict(sense_vectors)    
+
 def tokens_to_ids_by_sense(lemma):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     def idized_vecs(lemma):
@@ -302,6 +336,87 @@ def sample_inputids_pairs(n_pairs, lemma, sense1, sense2, n_fold, train_percent 
             test = sample(sense1_vecs_test, sense2_vecs_test)
 
             data.append((train, test))
+        return data
+
+def sample_sense_pairs_with_vec(vectorization, n_pairs, lemma, sense1, sense2, n_fold, train_percent = 0.8):
+    """
+    Creates training and test data for classifying whether two SenseInstances
+    of a particular lemma correspond to the same sense (positive) or a
+    different sense (negative).
+    
+    First, the SenseInstances for the specified senses are split into train
+    and test, according to the train_percent. Then we randomly generate n 
+    positive training examples and n negative training examples.
+    
+    """
+    
+    def sample_negative_sense_pair(sense_vecs):
+        assert len(sense_vecs) >= 2, "sample_negative_sense_pair cannot be called with only one sense!"
+        sense1, sense2 = random.sample(list(range(len(sense_vecs))), 2)
+        vec1 = random.choice(sense_vecs[sense1])
+        vec2 = random.choice(sense_vecs[sense2])
+        return torch.cat([vec1, vec2])
+       
+    def sample_positive_sense_pair(sense_vecs):
+        # Currently may break if every sense doesn't have at least two vectors.
+        assert len(sense_vecs) >= 2, "sample_positive_sense_pair cannot be called with zero senses!"
+        sense = random.choice(list(range(len(sense_vecs))))
+        vec1, vec2 = random.sample(sense_vecs[sense], 2)
+        return torch.cat([vec1, vec2])
+
+    def train_test_split(vecs, percent_train):
+        random.shuffle(vecs)
+        cutoff = int(percent_train * len(vecs))
+        return vecs[:cutoff], vecs[cutoff:]
+    
+    def train_test_split_nfold(vecs, percent_train, nthfold, n_fold):
+        random.shuffle(vecs)
+        cutoff_start = int((nthfold / n_fold) * len(vecs))
+        cutoff_end = int(((nthfold + 1) / n_fold) * len(vecs))
+        return ((vecs[:cutoff_start] + vecs[cutoff_end:]), vecs[cutoff_start:cutoff_end])
+
+
+    def sample(sense1_vecs, sense2_vecs):
+        vecs = [sense1_vecs, sense2_vecs]
+        positives = torch.stack([sample_positive_sense_pair(vecs) for 
+                                 _ in range(n_pairs)])
+        negatives = torch.stack([sample_negative_sense_pair(vecs) for
+                                 _ in range(n_pairs)])
+        positives = torch.cat([torch.ones(len(positives), 1), positives], 
+                               dim=1)
+        negatives = torch.cat([torch.zeros(len(negatives), 1), negatives], 
+                               dim=1)
+        return torch.cat([negatives, positives])
+
+    vecs_by_sense = contextualized_vectors_by_sense_with_vec(lemma, vectorization)
+    sense1_vecs = vecs_by_sense[sense1]
+    sense2_vecs = vecs_by_sense[sense2]
+    print('sampling {} with senses of magnitude {} and {}'.format(lemma, 
+          len(sense1_vecs), len(sense2_vecs)))
+
+    if n_fold == 1:
+        sense1_vecs_train, sense1_vecs_test = train_test_split(sense1_vecs,
+                                                               train_percent) 
+        sense2_vecs_train, sense2_vecs_test = train_test_split(sense2_vecs,
+                                                               train_percent)
+        train = sample(sense1_vecs_train, sense2_vecs_train)
+        test = sample(sense1_vecs_test, sense2_vecs_test)
+
+        return [(train, test)]
+    elif n_fold > 1:
+        data = []
+        for i in range(n_fold):
+            sense1_vecs_train, sense1_vecs_test = train_test_split_nfold(sense1_vecs,
+                                                               train_percent,
+                                                               i, n_fold) 
+            sense2_vecs_train, sense2_vecs_test = train_test_split_nfold(sense2_vecs,
+                                                               train_percent,
+                                                               i, n_fold)
+            train = sample(sense1_vecs_train, sense2_vecs_train)
+            test = sample(sense1_vecs_test, sense2_vecs_test)
+
+            data.append((train, test))
+
         return data
 
 
