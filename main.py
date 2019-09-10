b4883e42e7b073913b387ac2c3720b3b6c514b4b
Kai <kai.pinckard@gmail.com>
2019-09-10 13:11:29 -0700

    prepare to run finetune with updating results

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from experiment import train_finetune, train_cross_lemmas, neighbors_test, train_lemma_classifiers_with_vec_elmo
 from elmo import elmo_vectorize
 def run_main():
-    train_finetune(21,21, 10, 20)
+    train_finetune(21, 10000000000, 5, 2000)
 
 if __name__ == "__main__":
-    d = train_lemma_classifiers_with_vec_elmo(elmo_vectorize, 21, 100000000000, 5, 2000)
+    #d = train_lemma_classifiers_with_vec_elmo(elmo_vectorize, 21, 100000000000, 5, 2000)
+    run_main()
