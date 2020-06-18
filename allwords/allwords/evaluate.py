b49bb708849f523a56c554d5334284f66fbe6abf
Mark Hopkins <hopkinsm@reed.edu>
2020-06-18 11:39:44 -0600

    Refactored decode so that it returns confidence levels.

--- allwords/allwords/evaluate.py
+++ allwords/allwords/evaluate.py
@@ -38,6 +38,13 @@ def apply_zone_masks(outputs, zones):
     return revised
 
 def decode(net, data):
+    """
+    Runs a trained neural network classifier on validation data, and iterates
+    through the top prediction for each datum.
+    
+    TODO: write some unit tests for this function
+    
+    """
     net.eval()
     val_loader = data.batch_iter()
     for inst_ids, targets, evidence, response, zones in val_loader:
@@ -52,32 +59,6 @@ def decode(net, data):
             yield inst_id, target, prediction, gold, confidence
     net.train()
 
-"""
-def decode(net, data, threshold=LARGE_NEGATIVE):
-    
-    Runs a trained neural network classifier on validation data, and iterates
-    through the top prediction for each datum.
-    
-    TODO: write some unit tests for this function
-    
-       
-    net.eval()
-    val_loader = data.batch_iter()
-    for inst_ids, targets, evidence, response, zones in val_loader:
-        val_outputs = net(evidence)
-        val_outputs = F.softmax(val_outputs, dim=1)
-        revised = apply_zone_masks(val_outputs, zones)
-        maxes, preds = revised.max(dim=-1)
-        below_threshold_idx = (maxes < threshold)
-        preds[below_threshold_idx] = ABSTAIN
-        for element in zip(inst_ids, 
-                           zip(targets,
-                               zip(preds, response.tolist()))):                    
-            (inst_id, (target, (prediction, gold))) = element
-            yield inst_id, target, prediction, gold
-    net.train()
-"""
-
 def evaluate(net, data):
     """
     The accuracy (i.e. percentage of correct classifications) is returned.
@@ -89,35 +70,29 @@ def evaluate(net, data):
     correct, total = accuracy(predictions, gold)
     return correct/total
 
-
-def py_curve(predictor, gold, thresholds):
-    pys = {}
-    for thres in thresholds:
-        preds = predictor(thres)
-        n_correct, n_confident = accuracy(preds, gold)
-        precision = 0 if n_confident == 0 else n_correct / n_confident
-        n_correct, n_all = yielde(preds, gold)
-        y = n_correct / n_all
-        pys[thres] = (precision, y)
-    return pys    
-
-
 def precision_yield_curve(net, data):
+    """
+    This might not work. Test on some actual data then remove this comment!
+    
+    """
     decoded = list(decode(net, data))
     decoded.sort(key = lambda t: t[4]) # sort decoded by confidence
-    preds = [p for (_, _, p, _, _) in decoded]
+    preds = [p.item() for (_, _, p, _, _) in decoded]
     gold = [g for (_, _, _, g, _) in decoded]
-    confidences = [c for (_, _, _, _, c) in decoded]
+    confidences = [c.item() for (_, _, _, _, c) in decoded]
+    return py_curve(preds, gold, confidences)
+
+def py_curve(preds, gold, confidences):
     pr_curve = {}
     for c in confidences:
-        pr_curve[c.item()] = [0,0] # first is n_correct, second n_confident
+        pr_curve[c] = [0,0] # first is n_correct, second n_confident
     for thres in pr_curve:
         n_correct = 0
         n_confident = 0
         for i in range(len(preds)):
-            p = preds[i].item()
+            p = preds[i]
             g = gold[i]
-            c = confidences[i].item()
+            c = confidences[i]
             is_correct = (p == g)
             if c >= thres:
                 n_confident += 1
@@ -138,8 +113,7 @@ def plot_py_curve(py_curve):
     ax1.set_ylabel('precision')
     ax1.set_xlabel('recall')
     ax1.plot(x, y)
-
-    
+  
 def save_py_curve(curve):
     confidence_path = join(file_dir, "../confidence")
     if not os.path.isdir(confidence_path):
